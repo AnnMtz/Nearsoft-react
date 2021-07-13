6e829941add2f88ea9c2acb19b72baa9477671de
AnnMtz <amonmtz009@gmail.com>
2021-07-13 15:21:43 -0500

    subiendo modificaciones

--- src/App.js
+++ src/App.js
@@ -11,43 +11,7 @@ function App() {
 
   //se añade un state para controlar lo que se consulta
   const [consultar, guardarConsultar] = useState(false);
-
-  const [resultado, guardarResultado] = useState([]);
-
-  const [text, setText] = useState('');
-
-  const [search, setSearch] = useState([]);
-
-  //extraigo los valores de title del api
-  const {title} = busqueda;
-
-  //se crea un useEffect para ver los cambios que vaya teniendo
-  useEffect(() => {
-    const consultarAPI = async () => {
-      const api = await axios.get('https://api.github.com/search/issues?q=repo:facebook/react+debug:in:title');
-      console.log(api.data.items);
-      guardarResultado(api.data.items)
-    }
-  consultarAPI();
-  }, []);
-
-  const onSuggestHandler = (text) => {
-    setText(text);
-    setSearch([]);
-  }
-
-  const onChangeHandler = (text) => {
-    let matches = [];
-    if(text.length>0){
-      matches = resultado.filter(item => {
-        return item.title
-      })
-    }
-    console.log('matches', matches);
-    setSearch(matches)
-    setText(text)
-  }
-
+  
   return (
     <div className="App">
       <Header 
